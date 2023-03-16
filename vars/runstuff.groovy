9d94126ad92f88b8905271cef5c634b5e6c5a34e
Christine Caulfield <ccaulfie@redhat.com>
2023-03-16 14:12:32 +0000

    config

--- vars/runstuff.groovy
+++ vars/runstuff.groovy
@@ -1,4 +1,6 @@
 def call(Map config = [:]) {
   sh "echo Prep to run pipeline for ${config.project}, branch ${config.branch}, opts: ${config.makeopts}"
+  sh "sh autogen.sh"
+  sh "./configure"
   sh "make ${config.makeopts}"
 }
