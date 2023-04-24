4fe02a0f39c9f130479726e57874bfdf4674e506
Christine Caulfield <ccaulfie@redhat.com>
2023-04-24 08:10:26 +0100

    !rust

--- vars/clusterLibSendReply.groovy
+++ vars/clusterLibSendReply.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
     // TODO make this site-agnostic so that it can deal with Pagure as well as github
-    if config.platform == "github" {
+    if (config.platform == "github") {
 	pullRequest.Comment(config.msg);
     } else {
 	echo "Platform ${config.platform} not configure in clusterLibSendReply.groovy"
