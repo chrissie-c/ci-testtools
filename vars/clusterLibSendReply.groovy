60b28d3c136190331e5614fff6624ce76332e83a
Christine Caulfield <ccaulfie@redhat.com>
2023-04-24 08:40:22 +0100

    clueless

--- vars/clusterLibSendReply.groovy
+++ vars/clusterLibSendReply.groovy
@@ -1,7 +1,7 @@
 def call(String platform, String msg) {
     // TODO make this site-agnostic so that it can deal with Pagure as well as github
     if (platform == "github") {
-	comment = "${msg}"
+	comment = '$msg'
 	pullRequest.Comment(comment);
     } else {
 	echo "Platform ${platform} not configure in clusterLibSendReply.groovy"
