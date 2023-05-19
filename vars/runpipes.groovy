f1985de8491f5a35f8d73baf7a6a26dcf7194a63
Christine Caulfield <ccaulfie@redhat.com>
2023-05-19 10:06:30 +0100

    test parallel

--- vars/runpipes.groovy
+++ vars/runpipes.groovy
@@ -1,57 +1,54 @@
 def call(Map pipelineParams = [:]) {
-  
-  pipeline {
+    
+    pipeline {
 	agent none
-	options { 
-	    disableConcurrentBuilds() 
+	stages {
+            stage('Build and Test') {
+		environment {
+	    	    PROJECT = "${pipelineParams.project}"
+		    BRANCH = "${pipelineParams.branch}"
+		}
+		matrix {
+                    agent {
+			label "${PLATFORM}"
+                    }
+                    axes {
+			axis {
+                            name 'PLATFORM'
+                            values 'fedora', 'debian', 'rhel'
+			}
+                    }
+                    stages {
+			stage('Prep') {
+		            steps {
+				sh "sh autogen.sh"
+				sh "./configure"
+			    }
+			}
+			stage('Build') {
+                            steps {
+				runstuff(project:"$PROJECT", branch:"$BRANCH", makeopts:"all")
+			    }
+			}
+			stage('Test') {
+                            steps {
+				runstuff(project:"$PROJECT", branch:"$BRANCH", makeopts:"check")
+			    }
+			}
+			stage('Build tarball') {
+                            steps {
+				runstuff(project:"$PROJECT", branch:"$BRANCH", makeopts:"dist")
+  				archiveArtifacts artifacts: "$PROJECT*.tar.gz, $PROJECT*.tar.xz", fingerprint: false
+			    }
+			}
+		    }
+		    post {
+			always {
+			    sh "uname -a"
+			}
+		    }
+		}
+	    }
 	}
-      stages {
-        stage('Build and Test') {
-	    environment {
-	    		PROJECT = "${pipelineParams.project}"
-			BRANCH = "${pipelineParams.branch}"
-	    }
-            matrix {
-                agent {
-                    label "${PLATFORM}"
-                }
-                axes {
-                    axis {
-                        name 'PLATFORM'
-                        values 'fedora', 'debian', 'rhel'
-                    }
-                }
-                stages {
-                    stage('Prep') {
-		        steps {
-		            sh "sh autogen.sh"
-			    sh "./configure"
-			}
-		    }
-                    stage('Build') {
-                        steps {
-			    runstuff(project:"$PROJECT", branch:"$BRANCH", makeopts:"all")
-			}
-                    }
-                    stage('Test') {
-                        steps {
-			    runstuff(project:"$PROJECT", branch:"$BRANCH", makeopts:"check")
-			}
-                    }
-		    stage('Build tarball') {
-                        steps {
-			    runstuff(project:"$PROJECT", branch:"$BRANCH", makeopts:"dist")
-  		            archiveArtifacts artifacts: "$PROJECT*.tar.gz, $PROJECT*.tar.xz", fingerprint: false
-		    }
-                }
-	    }
-            post {
-              always {
-	        sh "uname -a"
-            }
-          }
-        }
-      }
     }
-  }
 }
