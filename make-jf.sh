1c69fb8cd8cf399c99d17b08bf7b0c009ff0bd54
Christine Caulfield <ccaulfie@redhat.com>
2023-06-02 13:13:24 +0100

    Exclude anvils

--- make-jf.sh
+++ make-jf.sh
@@ -3,7 +3,7 @@
 # Takes a template Jenkinsfile (in $1) and adds
 # the node axes for voting,nonvoting and staticcheck
 #
-
+HOME=~jenkins
 
 getnodelist() {
 	nodelist=$(echo $(ls -1 $HOME/nodes) | tr '\n' ' ')
@@ -11,21 +11,50 @@ getnodelist() {
 
 add_to_functions() {
 	labels=$(cat $HOME/nodes/$1/config.xml | grep label | sed -e 's/<label>//g' -e 's/<\/label>//g')
+	added="0"
+	anvil="0"
 
+	# Check for anvil nodes first - so we can exclude them
 	for i in $labels
 	do
 	    case $i in
-		voting)
-		    FUNCTION["voting"]+=$1
-		    ;;
-		non-voting)
-		    FUNCTION["nonvoting"]+=$1
-		    ;;
-		coverity)
-		    FUNCTION["coverity"]+=$1
+		anvil*)
+		    anvil="1"
 		    ;;
 	    esac
 	done
+
+	# Now do the main functions
+	if [ "$anvil" = "0" ]
+	then
+	    for i in $labels
+	    do
+		case $i in
+		    voting)
+			FUNCTION["voting"]+=$1
+			added=1
+			;;
+		    nonvoting)
+			FUNCTION["nonvoting"]+=$1
+			added=1
+			;;
+		    coverity)
+			FUNCTION["coverity"]+=$1
+			added=1
+			;;
+		esac
+	    done
+
+	    # Add all used nodes to the matrix nodelist
+	    if [ "$added" = "1" ]
+	    then
+		if [ -n "${ALLNODES}" ]
+		then
+		    ALLNODES+=", "
+		fi
+		ALLNODES+="'$1'"
+	    fi
+	fi
 }
 
 if [ -z "$1" ]
@@ -45,18 +74,24 @@ fi
 getnodelist
 declare -A EXCLUDES
 declare -A FUNCTION
-ALLFUNCS="voting,nonvoting,staticcheck,cleanup"
 
+# Build the function/node list
 for i in $nodelist
 do
     add_to_functions $i
 done
 
 # Build the excludes list for the Jenkinsfile
-for i in "voting" "nonvoting" "staticcheck"
+for i in voting nonvoting staticcheck
 do
+    if [ -n "${ALLFUNCS}" ]
+    then
+	ALLFUNCS+=","
+    fi
+    ALLFUNCS+="'"${i}"'"
+
     EXCLUDES[$i]=""
-    for n in $nodelist
+    for n in $ALLNODES
     do
 	node=`echo $n|tr -d ",' "`
 	echo "${FUNCTION[$i]}" | grep "$node" > /dev/null 2>/dev/null
@@ -71,7 +106,8 @@ do
     done
 done
 
-sed < $1 -e "s/<<PLATFORMS>>/$nodelist/" \
+# Build the new Jenkinsfile
+sed < $1 -e "s/<<PLATFORMS>>/$ALLNODES/" \
     -e"s/<<FUNCTIONS>>/$ALLFUNCS/" \
     -e"s/<<EXCLUDES-voting>>/${EXCLUDES['voting']}/" \
     -e"s/<<EXCLUDES-staticcheck>>/${EXCLUDES['staticcheck']}/" \
