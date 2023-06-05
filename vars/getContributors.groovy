bde97bb7f11ff623c2cf4d68bde89c434e4c97db
Christine Caulfield <ccaulfie@redhat.com>
2023-06-05 13:06:15 +0100

    cred

--- vars/getContributors.groovy
+++ vars/getContributors.groovy
@@ -6,7 +6,7 @@ def call(String owner, String repo)
     // Make an HTTP request to the GitHub API
     def response = httpRequest(
         url: apiUrl,
-        authentication: 'github',
+        authentication: 'chrissie-c',
         validResponseCodes: '200',
         contentType: 'APPLICATION_JSON'
     )
