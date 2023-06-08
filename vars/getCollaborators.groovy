67245e9e155055f35b3242f80292c9fc3658bfce
Christine Caulfield <ccaulfie@redhat.com>
2023-06-08 14:32:24 +0100

    tests

--- vars/getCollaborators.groovy
+++ vars/getCollaborators.groovy
@@ -1,26 +1,34 @@
-def call(String url)
+def call(String github_url)
 {
     // Turn the URL into project/repo
-    s = url.split("/")
+    s = github_url.split("/")
     owner = s[3]    
-    repo = s[4].substring(0,s[4].length()-4)
+    repo = s[4].substring(0, s[4].length()-4)
     url = "https://api.github.com/repos/${owner}/${repo}/collaborators"
 
-    println("CC: GOT HERE for ${url}")
-    COLLAB_CREDENTIALS = credentials("489d4ea3-11c9-40ab-a794-54197b51f081")
-    println('$COLLAB_CREDENTIALS')
+    println("CC: NOW GOT HERE for ${url}")
+    CREDS = credentials("489d4ea3-11c9-40ab-a794-54197b51f081")
+    println('$CREDS')
 
     collabs = sh (
-	script: "curl -s -u '${COLLAB_CREDENTIALS}'" + " ${url}",
+	script: 'curl -s -u ' + CREDS  + " ${url}",
 	returnStdout: true)
-    parsed = readJSON text: collabs
-    len=5; //yeah
-    println("{$collabs}")
+    parsed = jsonSlurper.parse(collabs)
+    println("$parsed")
     
-    String[] collabs;    
-    for (i=0; i<len; i++ ) {
-	println(parsed.login)
-	collabs[i] = parsed.login
+    println(parsed.login)
+    cnum = parsed.login.size()
+    println(cnum)
+    
+    collabs = [];
+    for (i=0; i<cnum; i++ ) {
+        println(parsed.login[i])
+        println(parsed.role_name[i])
+        if ((parsed.role_name[i] == "admin") || (parsed.role_name[i] == "write")) {
+            println("Adding this one")
+            collabs.add(parsed.login[i])
+	}
     }
     return collabs
+
 }
