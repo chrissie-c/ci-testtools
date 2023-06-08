23840ae307304ce974c58f671c8cd60ad0fd7737
Christine Caulfield <ccaulfie@redhat.com>
2023-06-08 13:50:24 +0100

    hih

--- vars/getCollaborators.groovy
+++ vars/getCollaborators.groovy
@@ -6,6 +6,7 @@ def call(String url)
     repo = s[4].substring(0,s[4].length()-4)
     url = "https://api.github.com/repos/${owner}/${repo}/collaborators"
 
+    println("CC: GOT HERE for ${url}")
     COLLAB_CREDENTIALS = credentials("489d4ea3-11c9-40ab-a794-54197b51f081")
     println("${COLLAB_CREDENTIALS}")
     println("${COLLAB_CREDENTIALS_USR}")
