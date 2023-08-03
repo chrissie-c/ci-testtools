1c77fc815771b277497083d6687440532ff41124
Christine Caulfield <ccaulfie@redhat.com>
2023-08-03 15:27:07 +0100

    sigh

--- vars/testTimeout.groovy
+++ vars/testTimeout.groovy
@@ -1,11 +1,8 @@
 def call(Exception err)
 {
-    def user = err.getCauses()[0].getUser()
+//    def user = err.getCauses()[0].getUser()
     println("Caught "+err)
-    println("user: "+user);
+//    println("user: "+user);
     println("causes: "+err.getCauses());
     println("causes[0]: "+err.getCauses()[0]);
-    if('SYSTEM' == user.toString()) {
-	println("yeeh, it was SYSTEM")
-    }
 }
