040f9d67d1dd6d620f374d9cc1a04065a1c68097
Christine Caulfield <ccaulfie@redhat.com>
2023-08-04 07:35:31 +0100

    143

--- vars/testTimeout.groovy
+++ vars/testTimeout.groovy
@@ -1,17 +1,27 @@
-def call(Integer time, String cmd)
+ def call(Integer time, String cmd)
 {
     def retval = 0
-    println("In testTimeout")
-
     
     try {
-	timeout(time: time, unit: 'SECONDS' ) {
+	timeout(time: time, unit: 'SECONDS') {
 	    sh "${cmd}"
 	}
     } catch (hudson.AbortException ae) { // Script Error
-	println("Script error: ${ae}")
-	retval = 1
+	// This is actually an abort
+	// https://gist.github.com/stephansnyt/3ad161eaa6185849872c3c9fce43ca81?permalink_comment_id=2198976
+	if (ae.getMessage().contains('script returned exit code 143')) {
+	    println('"code 143" abort')
+	    retval = 3
+	} else {
+	    println('Script exit')
+	    retval = 1
+	}
     } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException err) {
+	// If no 'cause' is given then we don't know what happened
+	// so just rethrow it.
+	if (err.getCauses() == 0) {
+	    throw (err)
+	}
 	def String cause = err.getCauses()[0]
 	if (cause.startsWith('org.jenkinsci.plugins.workflow.steps.TimeoutStepExecution$ExceededTimeout')) {
 	    println('Timeout exceeded')
@@ -20,6 +30,9 @@ def call(Integer time, String cmd)
 	else if (cause.startsWith('jenkins.model.CauseOfInterruption$UserInterruption')) {
 	    println('User abort')
 	    retval = 3
+	} else {
+	    // Not for us - rethrow it
+	    throw(err)
 	}
     }
     return retval
