cd2bb9e14ae12d7035c44e678a57331542ea0976
Christine Caulfield <ccaulfie@redhat.com>
2023-08-10 10:58:03 +0100

    again

--- vars/getSCM.groovy
+++ vars/getSCM.groovy
@@ -3,9 +3,9 @@ def call()
     if (env.NODE_NAME == 'built-in') {
 	dir ('project') {
 	    checkout scm
-	    sh 'tar -czf gitfiles.tgz project'
-	    archiveArtifacts artifacts: "gitfiles.tgz", fingerprint: false
 	}
+	sh 'tar -czf gitfiles.tgz project'
+	archiveArtifacts artifacts: "gitfiles.tgz", fingerprint: false
     } else {
 	sh "wget ${env.BUILD_URL}artifact/gitfiles.tgz"
 	sh "tar -xzf gitfiles.tgz"
