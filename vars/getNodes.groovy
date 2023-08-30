2aeb2ff17c076e82863816b2b178ed8b12cd266d
Christine Caulfield <ccaulfie@redhat.com>
2023-08-30 10:01:18 +0100

    for

--- vars/getNodes.groovy
+++ vars/getNodes.groovy
@@ -1,12 +1,12 @@
 @NonCPS
 def call(String label)
 {
-    jenkins.model.Jenkins.instance.nodes.collect { thisAgent ->
+    def nodelist = []
+    for thisAgent in jenkins.model.Jenkins.instance.nodes {
 	labelarray = thisAgent.labelString.split(' ')
-	println("CC: test: "+thisAgent.name)
         if (labelarray.contains(label)) {
-            return thisAgent.name
+            nodelist += thisAgent.name
         }
     }
+    return nodelist
 }
-
