123df78a8733c86287dd46ad7d7bface5e1eef25
Christine Caulfield <ccaulfie@redhat.com>
2023-08-30 09:52:36 +0100

    yes

--- vars/getNodes.groovy
+++ vars/getNodes.groovy
@@ -4,7 +4,6 @@ def call(String label)
     jenkins.model.Jenkins.instance.nodes.collect { thisAgent ->
        labelarray = thisAgent.labelString.split(' ')
         if (labelarray.contains(label)) {
-	    println("got ${thisAgent.name}"
             return thisAgent.name
         }
     }
