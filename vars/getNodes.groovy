dca4cd382572bfa4cc985ce52a41dfe178629892
Christine Caulfield <ccaulfie@redhat.com>
2023-08-30 09:54:20 +0100

    !null

--- vars/getNodes.groovy
+++ vars/getNodes.groovy
@@ -3,7 +3,7 @@ def call(String label)
 {
     jenkins.model.Jenkins.instance.nodes.collect { thisAgent ->
        labelarray = thisAgent.labelString.split(' ')
-        if (labelarray.contains(label)) {
+        if (labelarray != null && labelarray.contains(label)) {
             return thisAgent.name
         }
     }
