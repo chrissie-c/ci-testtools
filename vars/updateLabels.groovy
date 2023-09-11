df2ff5205e64d67ea72d9958d8c4b4be4f10c348
Christine Caulfield <ccaulfie@redhat.com>
2023-09-11 10:23:06 +0100

    str

--- vars/updateLabels.groovy
+++ vars/updateLabels.groovy
@@ -1,31 +1,31 @@
 import Jenkins.instance.*
 
 def call()
-{    
+{
     println("CC: updateLabels")
     def nodes = getNodes()
     def newlabels_arr = nodes[env.NODE_NAME]
 
     println("CC: Update Labels on ${env.NODE_NAME} - array="+newlabels_arr)
-    
+
     // Don't update built-in or other nodes with no labels in getNodes()
     if (newlabels_arr == null || newlabels_arr.size() == 0) {
 	return
     }
-    
+
     // Convert the array to a long string
     def newlabels_str = ''
     for (i in newlabels_arr) {
 	newlabels_str += " ${i}"
     }
-    
+
     // Get the existing labels, so we don't lose "down" if it's present
     def curlabels = Jenkins.instance.getNode("rhel").getLabelString().split()
     if (curlabels.contains('down')) {
 	newlabels_str += " down"
     }
     println("CC: old labels: ${curlabels}")
-    println("CC: new labels: ${newlabels}")    
+    println("CC: new labels: ${newlabels_str}")
 
     // Update Jenkins
     node_handle.setLabelString(newlabels_str)
