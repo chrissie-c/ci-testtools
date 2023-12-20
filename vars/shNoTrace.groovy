47d02f6037fce998f7ea54d5bc7d57cb14e39e1d
Christine Caulfield <ccaulfie@redhat.com>
2023-12-20 10:25:32 +0000

    fnesse

--- vars/shNoTrace.groovy
+++ vars/shNoTrace.groovy
@@ -1,4 +1,4 @@
 def call(String cmd, String visible) {
-    def a = steps.sh (script: '#!/bin/sh -e\n'+cmd, returnStdout: true, label: visible)
+    def a = sh (script: '#!/bin/sh -e\n'+cmd, label: visible)
     return a
 }
