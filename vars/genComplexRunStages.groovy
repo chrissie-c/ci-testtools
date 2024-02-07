a2c7e51e787e8f083c025f9c91e6897a549e39b8
Christine Caulfield <ccaulfie@redhat.com>
2024-02-07 15:01:38 +0000

    test complex gen

---
+++ vars/genComplexRunStages.groovy
@@ -0,0 +1,82 @@
+// Generate the jobs
+def call(Boolean do_zstream)
+{
+    // These could be parameters to the call....
+    def providers = [:]
+    providers['osp'] = ['maxjobs': 255, 'fatal': false, 'testlevel': 'smoke']
+    providers['aws'] = ['maxjobs': 1, 'fatal': true, 'testlevel': 'all']
+    providers['ibmvpc'] = ['maxjobs': 2, 'fatal': true, 'testlevel': 'all']
+
+    def versions = ['rhel8 next-stable', 'rhel9 next-stable', 'rhel9 main']
+    def branches = ['nightly','zstream']
+
+    // Build a list of possible jobs
+    def jobs = []
+    for (v in versions) {
+	def j = v.split(' ')
+	for (b in branches) {
+	    jobs += "${j[0]} ${b} ${j[1]}"
+	}
+    }
+
+    // List of things to run on all providers
+    def runjobs = [:]
+
+    for (p in providers) {
+	def pinfo = providers[p.key]
+	def maxjobs = pinfo['maxjobs'] // max parallel
+	def jobs_per_stage = Math.round((jobs.size() / maxjobs) + 0.5)
+	println("jobs per stage for ${p.key}: ${jobs_per_stage} (max concurrent = ${maxjobs})")
+
+	// Build the jobs
+	def s = 0
+	while (s < jobs.size()) {
+	    def joblist = []
+	    for (i=0; i < jobs_per_stage &&  s < jobs.size(); i++) {
+		joblist += "${jobs[s]}"
+		s += 1
+	    }
+	    println(joblist)
+	    // TODO this should actually be a call runComplexStage()
+	    // To be fed into 'parallel'
+	    //runjobs["${p.key} ${s}"] = ['provider': p.key, 'pinfo': pinfo, 'jobs': joblist, 'zstream': do_zstream]
+	    runjobs["${p.key} ${s}"] = { runComplexStage(['provider': p.key, 'pinfo': pinfo, 'jobs': joblist, 'zstream': do_zstream]) }
+	}
+    }
+    return runjobs
+}
+
+// This fn might be in a separate file (but in the same library)
+def runComplexStage(Map stageinfo)
+{
+    def provider = stageinfo['provider']
+    def pinfo = stageinfo['pinfo']
+    def running = true
+
+    println("runComplexStage: ${stageinfo}")
+
+    for (s in stageinfo['jobs']) {
+	if (running) {
+	    stage("${s} Smoke") {
+		def result = sh "echo ${provider} ${s} smoke"
+		if (result != 0 && provider['fatal'] == true) {
+		    running = false
+		}
+
+	    }
+	    // If that succeeds and provider allows 'all' then run all
+	    if (result == 0 && provider['testlevel'] == 'all') {
+		stage("${s} All") {
+		    def result = sh "echo ${provider} ${s} all"
+		    result = doall()
+		}
+		if (result != 0) {
+		    running = false
+		}
+	    }
+	}
+    }
+}
+
+
+def jobs = call(true)
