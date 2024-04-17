24301b44155490a3655f86ee5cc5ef55a2e2d74c
Christine Caulfield <ccaulfie@redhat.com>
2024-04-17 10:07:01 +0100

    ()

--- vars/doRunJob.groovy
+++ vars/doRunJob.groovy
@@ -2,13 +2,13 @@
 
 def call(String jobname, ArrayList params, Map info)
 {
-    def a = build job: jobname,
-	parameters: params,
-	propagate: false
-//	wait: false
+    def a = build(job: jobname,
+		  parameters: params,
+		  propagate: false,
+		  wait: false)
 
     println(a)
-    println(a.getNumber())
+
     info['joblist'] += a
 
     a.waitForBuild()
