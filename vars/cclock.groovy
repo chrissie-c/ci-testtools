cac6f3f67c93dd5166c35104e68a09be6c838682
Christine Caulfield <ccaulfie@redhat.com>
2025-02-19 10:36:11 +0000

    class

--- vars/cclock.groovy
+++ vars/cclock.groovy
@@ -3,17 +3,31 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
 
+class jnaflock {
+    public:
+    interface CLibrary extends Library {
+	CLibrary INSTANCE = (CLibrary)	Native.load("c", CLibrary.class);
+	
+	void printf(String format, Object... args);
+	int open(String file, Integer mode);
+	int creat(String file, int mode);
+	int flock(int fd, int mode);
+	int close(int fd);
+    }
+}
+
 
 def call(String lockname, String mode, Closure thingtorun)
 {
-    def lockmode = 0; // LOCK_SH
-    if (mode == "READ") {
+    def lockmode = 0;
+    if (mode == 'READ') {
 	lockmode = 1; // LOCK_SH
     }
-    if (mode == "WRITE") {
+    if (mode == 'WRITE') {
 	lockmode = 2; // LOCK_EX
     }
     if (lockmode == 0) {
+	println("Unknown lock mode ${mode}")
 	return -1
     }
     
