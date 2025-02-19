8e168abbeffaf0f679223140c5d8d7d29501d5db
Christine Caulfield <ccaulfie@redhat.com>
2025-02-19 10:32:26 +0000

    lock

--- vars/cclock.groovy
+++ vars/cclock.groovy
@@ -4,13 +4,23 @@ import com.sun.jna.Native;
 import com.sun.jna.Platform;
 
 
-import com.redhat.clusterci.jnaflock
-
-def call(String name)
+def call(String lockname, String mode, Closure thingtorun)
 {
-    def a = new jnaflock('tmp/test.lock')
-    a.lock('EX')
-    println(a)
-    sleep(5000)
-    a.unlock()
+    def lockmode = 0; // LOCK_SH
+    if (mode == "READ") {
+	lockmode = 1; // LOCK_SH
+    }
+    if (mode == "WRITE") {
+	lockmode = 2; // LOCK_EX
+    }
+    if (lockmode == 0) {
+	return -1
+    }
+    
+    def fd = jnaflock.CLibrary.INSTANCE.creat("/tmp/${lockname}", 0666)
+    println("FD for test.lock is " + fd)
+    jnaflock.CLibrary.INSTANCE.flock(fd, 1) // LOCK_SH
+    thingtorun()
+    jnaflock.CLibrary.INSTANCE.close(fd)
+    println("Closed")
 }
