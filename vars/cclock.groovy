f0aec2b477c8aad69f6e0d40c262916eb07519a8
Christine Caulfield <ccaulfie@redhat.com>
2025-03-03 07:42:13 +0000

    tetsing testing

--- vars/cclock.groovy
+++ vars/cclock.groovy
@@ -1,42 +1,82 @@
-// Just to see if this works
+// Provide READ/WRITE locking in Jenkins, using flock over jna
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Platform;
 
 class jnaflock {
-    interface CLibrary extends Library {
-	CLibrary INSTANCE = (CLibrary)	Native.load("c", CLibrary.class);
-	
-	void printf(String format, Object... args);
-	int open(String file, int mode);
+    // com.sun.jna.Library need to be named explicity for some reason.
+    interface CLibrary extends com.sun.jna.Library {
+	CLibrary INSTANCE = (CLibrary)Native.load("c", CLibrary.class);
+
 	int creat(String file, int mode);
 	int flock(int fd, int mode);
 	int close(int fd);
     }
 }
 
-
-def call(String lockname, String mode, Closure thingtorun)
+// Called in post {always{}} to make sure the file is closed and unlocked, called from main fn so
+// is already running on 'built-in'.
+def do_unlock(Map info)
 {
+    println("do_unlock: info map: ${info}")
+
+    if (info.containsKey('lockfd') && info['lockfd'] >= 0) {
+	if (jnaflock.CLibrary.INSTANCE.flock(info['lockfd'], 8) == -1) { // 8 = LOCK_UNLOCK
+	    println("do_unlock failed on fd ${info['lockfd']}")
+	    return
+	}
+	jnaflock.CLibrary.INSTANCE.close(info['lockfd'])
+	println("Lock on fd ${info['lockfd']} released")
+    }
+}
+
+def call(Map info, String lockname, String mode, Closure thingtorun)
+{
+    if (info['lockfd'] != -1) {
+	throw(new Exception("Request for lock ${lockname}, while lock on fd ${info['lockfd']} already held (only 1 lock allowed at a time)"))
+	return -1
+    }
+
+    def lockdir = "${JENKINS_HOME}/locks"
+
+    sh "mkdir -p ${lockdir}"
+
+    def lockmode = 0
+    if (mode == 'READ') {
+	lockmode = 1 // LOCK_SH
+    }
+    if (mode == 'WRITE') {
+	lockmode = 2 // LOCK_EX
+    }
+    if (mode == 'UNLOCK') {
+	do_unlock(info)
+	return -1 // Doesn't actually 'return' - more like a 'break' ???!!
+    }
+    if (lockmode == 0) {
+//	throw(new Exception("jnaflock: Unknown lock mode ${mode}"))
+	return -1
+    }
+    
     // This MUST run on the Jenkins host
     node('built-in') {
-	def lockmode = 0;
-	if (mode == 'READ') {
-	    lockmode = 1; // LOCK_SH
-	}
-	if (mode == 'WRITE') {
-	    lockmode = 2; // LOCK_EX
-	}
-	if (lockmode == 0) {
-	    println("jnaflock: Unknown lock mode ${mode}")
+	info['lockfd'] = jnaflock.CLibrary.INSTANCE.creat("${lockdir}/${lockname}.lock", 0666)
+	if (info['lockfd'] == -1) {
+	    throw(new Exception("Failed to 'creat' file for lock ${lockdir}/${lockname}"))
 	    return -1
 	}
-	
-	def fd = jnaflock.CLibrary.INSTANCE.creat("/tmp/${lockname}", 0666)
-	println("FD for test.lock is " + fd)
-	jnaflock.CLibrary.INSTANCE.flock(fd, lockmode)
-	thingtorun()
-	jnaflock.CLibrary.INSTANCE.close(fd)
-	println("Closed")
+	println("FD for lock ${lockname} is " + info['lockfd'])
+	jnaflock.CLibrary.INSTANCE.flock(info['lockfd'], lockmode)
     }
+
+    println("After first node() block")
+    
+    thingtorun()
+    
+    node('built-in') {
+	println("in second node() block")
+	jnaflock.CLibrary.INSTANCE.close(info['lockfd'])
+	println("Lock ${lockname} released")
+	info['lockfd'] = -1
+    }
+    return 0
 }
