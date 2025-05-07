41d236f8752a4382fb10f876991684a2a5e4a4c6
Christine Caulfield <ccaulfie@redhat.com>
2025-05-07 08:48:13 +0100

    rename

--- vars/RWLock.groovy
+++ vars/RWLock.groovy
@@ -1,6 +1,6 @@
-import org.jenkinsci.plugins.lockable_resources.LockableResourcesManager
-import org.jenkinsci.plugins.lockable_resources.locks.LockMode
-import org.jenkinsci.plugins.lockable_resources.queue.QueuedContext
+import org.jenkinsci.plugins.lockable_resources-plugin.LockableResourcesManager
+import org.jenkinsci.plugins.lockable_resources-plugin.locks.LockMode
+import org.jenkinsci.plugins.lockable_resources-plugin.queue.QueuedContext
 
 /**
  * Provides read/write locking functionality for Jenkins jobs using the Lockable Resources plugin.
