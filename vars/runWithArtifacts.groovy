7e71a431ded5c225c06513582d34bb13ddaa4793
Christine Caulfield <ccaulfie@redhat.com>
2026-03-16 08:41:39 +0000

    runWithArtifacts

---
+++ vars/runWithArtifacts.groovy
@@ -0,0 +1,24 @@
+
+
+
+// Run a groovy Closure and store the results in an artifact
+def call (Map info, String logfile, Closure cmd)
+{
+    def failed = 0
+    def new_logfile = ''
+    try {
+	tee (logfile) {
+	    cmd()
+	}
+    }
+    catch (e) {
+	failed = 1
+    }
+    if (failed == 1) {
+	new_logfile = "FAILED_${logfile}"
+
+    } else {
+	new_logfile = "SUCCESS_${logfile}"
+    }
+    sh "mv ${logfile} ${new_logfile}
+}
