4c607f48d38989ce905bd44cc6251e5cd33d3004
Christine Caulfield <ccaulfie@redhat.com>
2026-03-18 10:43:00 +0000

    tey

--- vars/getLogFile.groovy
+++ vars/getLogFile.groovy
@@ -5,8 +5,13 @@ def call()
 {
     println("in getLogFile")
 
-    def run = currentBuild.rawBuild
+    def root = currentBuild.rawBuild.getRootDir()
 
-    println(run)
-    println(run.getArtifactsDir());
+
+    def jobsdir1 = root.substring(0, root.lastIndexOf('/', root.lastIndexOf('/')-1)) // take it back to "builds"
+    def jobsdir2 = root.substring(0, root.lastIndexOf('/', root.lastIndexOf('/')-1)) // take it back to "brances"
+
+    println(jobsdir1)
+    println(jobsdir2)
 }
+
