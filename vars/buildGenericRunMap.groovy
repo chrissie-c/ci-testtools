0aba5bdea0d2cc70f98384164e3e81d71f2940f1
Christine Caulfield <ccaulfie@redhat.com>
2026-04-15 09:35:10 +0100

    return nodes only

--- vars/buildGenericRunMap.groovy
+++ vars/buildGenericRunMap.groovy
@@ -1,23 +1,6 @@
 
-def nodes=['debian', 'rhel', 'fedora']
-
-def runit(Map info, String agentName, Closure fn)
-{
-    node(agentName) {
-	fn()
-    }
-}
-
 // Generate a run-list
 def call(Map info, ArrayList nodes)
-{
-
-    def collectBuildEnv = [:]
-
-    for (def j in jobs) {
-	def job = j // needs to be local to the loop
-	collectBuildEnv[job] = ''
-    }
-
-    return collectBuildEnv
+{    
+    return nodes
 }
